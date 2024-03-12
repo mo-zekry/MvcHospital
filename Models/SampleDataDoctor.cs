f19e23b2194319dd6ffd1c1c9671865b67bf812a
mo-zekry <212103706@o6u.edu.eg>
2024-03-12 14:32:54 +0200

    init commit!

---
+++ Models/SampleDataDoctor.cs
@@ -0,0 +1,18 @@
+﻿namespace ContactDoctor.Models
+{
+    public class SampleDataDoctor
+    {
+        public List<Doctor> doctors;
+        public SampleDataDoctor()
+        {
+            doctors = new List<Doctor>
+            {
+            new() { Id = 1, Name = "Dr. John Smith", Specialization = "Cardiology", Img = "doctor1.jpg" },
+            new() { Id = 2, Name = "Dr. Sarah Johnson", Specialization = "Pediatrics", Img = "doctor2.jpg" },
+            new() { Id = 3, Name = "Dr. Emily Davis", Specialization = "Dermatology", Img = "doctor4.jpg" },
+            new() { Id = 4, Name = "Dr. Michael Lee", Specialization = "Orthopedics", Img = "doctor3.jpg" },
+            new() { Id = 5, Name = "Dr. William Clark", Specialization = "Neurology", Img = "doctor5.jpg" },
+            };
+        }
+    }
+}
